6f279586cefca34de1f121d5ad665172386737fc
Sean <sean.smith.2009@gmail.com>
2023-08-19 11:02:23 -0500

    Fix prettier issues

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -2,7 +2,7 @@
 const config = {
   plugins: [
     require.resolve("prettier-plugin-tailwindcss"),
-    "@trivago/prettier-plugin-sort-imports"
+    "@trivago/prettier-plugin-sort-imports",
   ],
 };
 
